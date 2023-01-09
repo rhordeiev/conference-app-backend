8e879bfcf71a151a6d022712dc00a5f1cdbf0889
Rostyslav Hordeiev <roshordeiev@gmail.com>
2023-01-09 15:45:00 +0200

    Headers change commit

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ use Application\Application;
 
 $app = new Application();
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Content-Type:application/json');
 $clientData = match ($_SERVER['REQUEST_METHOD']) {
     'GET' => $_GET,
